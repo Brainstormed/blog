c1e7574f5ea13141cbdd50573c764efa1a02dd2f
Brainstormed <aritramondal855@gmail.com>
2022-03-30 15:49:59 +0530

    commit 3

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const {
+    DateTime
+} = require("luxon")
+
 const eleventy = require("eleventy");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/')
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    })
 
     return {
         dir: {
